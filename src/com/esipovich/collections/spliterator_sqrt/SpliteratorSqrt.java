66e4af1f2561d28d1a16c4a95491568b79a8fba3
esipovich92 <esipovich92@gmail.com>
2018-05-07 14:23:55 +0300

    Spliterator - optimized solution

--- src/com/esipovich/collections/spliterator_sqrt/SpliteratorSqrt.java
+++ src/com/esipovich/collections/spliterator_sqrt/SpliteratorSqrt.java
@@ -3,7 +3,6 @@ package com.esipovich.collections.spliterator_sqrt;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Spliterator;
 
 /**
  * @author Artem Esipovich 5/7/2018
@@ -27,21 +26,15 @@ public class SpliteratorSqrt {
         print(spliteratorWork(Arrays.asList(4.0, 4.0, 4.0, 100.0, 100.0, 100.0)));
     }
 
-    public static List<Double> spliteratorWork(List<Double> lst){
+    private static List<Double> spliteratorWork(List<Double> lst){
         List<Double> sqrs = new ArrayList<>();
-        Spliterator<Double> spliterator = lst.spliterator();
-        while (spliterator.tryAdvance(e -> sqrs.add(Math.sqrt(e))));
+        lst.spliterator().forEachRemaining(e -> sqrs.add(Math.sqrt(e)));
         return sqrs;
     }
-    public static void print(List<Double> list){
-        Spliterator<Double> spliterator = list.spliterator();
-        Spliterator<Double> firstSpliterator = spliterator.trySplit();
-        while (firstSpliterator.tryAdvance(e -> {if (e >= 2) {
-            System.out.println(e);
-        } }));
+
+    private static void print(List<Double> list){
+        list.spliterator().trySplit().forEachRemaining(e -> {if (e >= 2) System.out.println(e);});
         System.out.println();
-        spliterator.forEachRemaining(e -> {if(e >= 10) {
-            System.out.println(e);
-        }});
+        list.spliterator().forEachRemaining(e -> {if (e >= 10) System.out.println(e);});
     }
 }
